418afa48bab5e39f44cc5b9239d2bc063ef3ad04
jlomako <71858760+jlomako@users.noreply.github.com>
2024-03-06 09:57:28 +0100

    Update and rename 2024_03_04_ER.R to app.R
    rename file, add content, change layout, change col names

--- app.R
+++ app.R
@@ -2,15 +2,22 @@ library(dplyr)
 library(ggplot2)
 library(shiny)
 
-# to do: select whole days 
-
-# get data - start with max
+# get data
 days <- 30
 getrows <- 24*days
+
+col_names <- c("Date", "Centre Hospitalier de l'Université de Montréal", "Centre Hospitalier de St. Mary", "CHU Sainte-Justine", "Hôpital de Lachine", "Hôpital de Lasalle", "Hôpital de Soins Psychiatriques de l'Est-de-Montréal", "Hôpital de Verdun", "Hôpital Douglas", "Hôpital du Sacré-Cœur de Montréal", "Hôpital en Santé Mentale Albert-Prévost", "Hôpital Fleury", "Hôpital Général de Montréal", "Hôpital Général du Lakeshore", "Hôpital Général Juif", "Hôpital Jean-Talon", "Hôpital Maisonneuve-Rosemont", "Hôpital Notre-Dame", "Hôpital Royal Victoria", "Hôpital Santa Cabrini", "Institut de Cardiologie de Montréal", "Hôpital de Montréal pour Enfants", "Total Montréal")
+
 file_path <- "https://github.com/jlomako/hospital-occupancy-tracker/raw/main/tables/"
-occupancy <- tail(vroom::vroom(paste0(file_path, "occupancy.csv"), show_col_types = FALSE), getrows)
-patients_total <- tail(vroom::vroom(paste0(file_path, "patients_total.csv"), show_col_types = FALSE), getrows)
-patients_waiting <- tail(vroom::vroom(paste0(file_path, "patients_waiting.csv"), show_col_types = FALSE), getrows)
+
+occupancy <- tail(vroom::vroom(paste0(file_path, "occupancy.csv"), show_col_types = F), getrows)
+patients_total <- tail(vroom::vroom(paste0(file_path, "patients_total.csv"), show_col_types = F), getrows)
+patients_waiting <- tail(vroom::vroom(paste0(file_path, "patients_waiting.csv"), show_col_types = F), getrows)
+
+# add column names
+colnames(occupancy) <- col_names
+colnames(patients_total) <- col_names
+colnames(patients_waiting) <- col_names
 
 # convert data
 occupancy <- as.data.frame(occupancy)
@@ -25,7 +32,7 @@ patients_waiting <- as.data.frame(patients_waiting)
 patients_waiting$Date <- as.POSIXct(patients_waiting$Date, format = "%Y-%m-%dT%H:%M")
 
 # get names
-hospitals <- names(occupancy[2:22])
+hospitals <- col_names[2:22]
 
 
 # get max
@@ -43,17 +50,18 @@ current_occupancy <- occupancy %>%
   arrange(desc(max_rate))
 
 
+
 ui <- bootstrapPage(
   
   # uses bootstrap 5
-  theme = bslib::bs_theme(version = 5, bootswatch = "sandstone"),
+  theme = bslib::bs_theme(version = 5, bootswatch = "spacelab"),
   
-  tags$head(HTML("<title>Montréal Emergency Room Tracker II</title>")),
+  tags$head(HTML("<title>Montréal Emergency Room Status</title>")),
   tags$script(HTML('https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js')),
   
   div(
     class = "container-sm px-0",
-    h1("Montréal Emergency Room Tracker", class = "text-center pt-3"),
+    h1("Montréal Emergency Room Status", class = "text-center pt-5"),
     
     div(
       class = "row",
@@ -65,34 +73,45 @@ ui <- bootstrapPage(
           class = "card h-100 text-center",
           
           div(
-            class = "card-header bg-secondary",
+            class = "card-header bg-primary",
             h5("Select a hospital to see current status:", class = "card-title")
           ),
           
-          div(
+          div(class="card-body py-2",
             selectInput(
               inputId = "hospital",
               label = NULL,
               choices = hospitals,
               width = "100%"
-            )
-          ),
-          
-          div(HTML("Some text about the current status of the selected hospital, patient counts, occupancy rate, wait time etc."), 
-              class="text-start"),
-          
-          div(
-            class = "card-body",
-            div(class = "row",
-                div(class = "col-sm-6 text-start",
+            ),
+
+            div(
+              class="px-0 text-start",
+              span(textOutput('selected_hospital_text'), class="pb-3 strong"),
+              div(
+                paste0("This information is updated every hour. The last update was on ", 
+                       format(most_recent_time, "%a, %b %e, %Y at %l %p"),
+                       ". For a detailed chronological representation of patient numbers and occupancy rates over time, please see the charts below."),
+                  class="py-3")
+                ),
+            
+            div(
+              h5(textOutput('current_hospital'), class = "py-2 border-top")
+            ),
+            
+            div(
+              class = "clearfix",
+                div(
+                  class = "float-start text-start",
                     div(h5("Hourly Occupancy Rate", class = "text-start pt-0 pb-2")),
                 ),
-                div(class="col-sm-6 text-end pb-0",
+                div(
+                  class="float-end text-end pb-0",
                     actionButton(
                       inputId = "decrease_days",
                       label = NULL,
                       icon = icon("minus"),
-                      class = "button btn btn-secondary btn-sm"
+                      class = "button btn btn-primary btn-sm"
                     ),
                     div(
                       class = "button btn btn-outline-secondary btn-sm",
@@ -102,10 +121,10 @@ ui <- bootstrapPage(
                       inputId = "increase_days",
                       label = NULL,
                       icon = icon("plus"),
-                      class = "button btn btn-secondary btn-sm"
-                    )
-                ), # end buttons
-            ), # end row
+                      class = "button btn btn-primary btn-sm"
+                    ),
+                 ), # end buttons
+             ), # end clearfix
             
             div(
               plotOutput('plot_occupancy')
@@ -121,11 +140,11 @@ ui <- bootstrapPage(
           div(
             class = "card-body",
 
-         #   div(class = "text-end py-0", textOutput('selected_hospital_display')),            
-            
             div(
-              h5("Hourly Patient Counts: ", class = "text-start pt-2 pb-0"), 
-              div(class = "text-end small",
+              class = "clearfix",
+              h5("Hourly Patient Counts", class = "text-start float-start"), 
+              div(
+                class = "small float-end",
                 span("Patients Total", style = "color: #121eff;"), 
                 span(" - "), 
                 span("Patients waiting", style = "color: #ff0000;")
@@ -155,7 +174,7 @@ ui <- bootstrapPage(
           class = "card h-100 text-start",
           
           div(
-            class = "card-header",
+            class = "card-header bg-secondary",
             h5("Compare Emergency room status", class = "card-title")
           ),
           
@@ -194,9 +213,27 @@ ui <- bootstrapPage(
 
 server <- function(input, output, session) {
   
-  # selected hospital
+  # text for selected hospital
   selected_hospital <- reactiveVal(hospitals[1])
-  output$selected_hospital_display <- renderText({ selected_hospital() })
+  # get current values for selected hospital and render text
+  output$selected_hospital_text <- renderText({
+    current_occupancy <- tail(occupancy[[selected_hospital()]], 1)
+    current_patients_waiting <- tail(patients_waiting[[selected_hospital()]], 1)
+    current_patients_total <- tail(patients_total[[selected_hospital()]], 1)
+    
+    # Check for NAs and show different text if any of them is.na
+    if (any(is.na(c(current_occupancy, current_patients_waiting, current_patients_total)))) {
+      "The data for the selected hospital is currently not available. Please check back later."
+    } else {
+      paste0(selected_hospital(), ": ",
+             "There are currently ", current_patients_total, " patients in the ER, with ",
+             current_patients_waiting, " of these patients waiting to be seen by a health professional. ",
+             "The current occupancy rate is ", current_occupancy, "%.")
+    }
+  })
+  
+  # name of selected hospital
+  output$current_hospital <- renderText({ selected_hospital() })
   
   # observe selected hospital
   observe({ selected_hospital(input$hospital) })
@@ -225,7 +262,7 @@ server <- function(input, output, session) {
     ggplot(selected_occupancy, aes(x = Date, y = !!sym(input$hospital))) +
         geom_line(linewidth = 0.5, show.legend = F, na.rm = T) +
         labs(x = NULL, y = NULL, title = NULL) +
-        scale_x_datetime(expand = c(0,0), date_labels = "%a, %b %d %H:%M", date_breaks = "1 day") +
+        scale_x_datetime(expand = c(0,0), date_labels = "%a, %b %e, %l%p", date_breaks = "1 day") +
         scale_y_continuous(expand = c(0,0), limits = c(0,occupancy_max_value), labels = scales::percent_format(scale = 1)) +
         theme_minimal() +
         geom_hline(yintercept = 100, linetype="dashed", col = "red") +
@@ -240,7 +277,7 @@ server <- function(input, output, session) {
       geom_line(data = selected_patients_total, aes(x = Date, y = !!sym(input$hospital), color = "Total"), linewidth = 0.5, show.legend = F, na.rm = T) +
       geom_line(data = selected_patients_waiting, aes(x = Date, y = !!sym(input$hospital), color = "Waiting"), linewidth = 0.5, show.legend = F, na.rm = T) +
       labs(x = NULL, y = NULL, title = NULL) +
-      scale_x_datetime(expand = c(0, 0), date_labels = "%a, %b %d %H:%M", date_breaks = "1 day") +
+      scale_x_datetime(expand = c(0, 0), date_labels = "%a, %b %e, %l%p", date_breaks = "1 day") +
       scale_y_continuous(expand = c(0, 0), limits = c(0, patients_total_max_value)) +
       theme_minimal() +
       theme(axis.text.x = element_text(angle = 90, hjust = 0.5, vjust = 0.5)) +
@@ -252,29 +289,3 @@ server <- function(input, output, session) {
 }
 
 shinyApp(ui, server)
-
-#   
-# 
-# # plot
-# ggplot(occupancy, aes(x = Date, y = occupancy[, 3])) +
-#   geom_line(size = 0.5, show.legend = F, na.rm = T) +
-#   labs(x = "Time", y = NULL, title = paste("Occupancy Rate: ",colnames(occupancy)[3])) +
-#   scale_x_datetime(expand = c(0,0), date_labels = "%a, %b %d %H:%M", date_breaks = "1 hour") +
-#   scale_y_continuous(expand = c(0,0), limits = c(0,occupancy_max_value), labels = scales::percent_format(scale = 1)) +
-#   theme_minimal() +
-#   geom_hline(yintercept = 100, linetype="dashed", col = "red") +
-#   theme(axis.text.x = element_text(angle=90, hjust=0.5, vjust=0.5))
-# 
-# # patients total and waiting
-# ggplot() +
-#   geom_line(data = patients_total, aes(x = Date, y = patients_total[, 3], color = "Total"), size = 0.5, show.legend = F, na.rm = T) +
-#   geom_line(data = patients_waiting, aes(x = Date, y = patients_waiting[, 3], color = "Waiting"), size = 0.5, show.legend = F, na.rm = T) +
-#   labs(x = "Time", y = NULL, title = paste0("Patients Waiting + Total: ",colnames(occupancy)[3])) +
-#   scale_x_datetime(expand = c(0, 0), date_labels = "%a, %b %d %H:%M", date_breaks = "1 hour") +
-#   scale_y_continuous(expand = c(0, 0), limits = c(0, patients_total_max_value)) +
-#   theme_minimal() +
-#   theme(axis.text.x = element_text(angle = 90, hjust = 0.5, vjust = 0.5)) +
-#   scale_color_manual(values = c("Total" = "blue", "Waiting" = "red"), name = "Dataset")
-# 
-# 
-
